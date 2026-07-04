68675b7b2704d1e516af3bcfb2eb06395bf97b87
dantheman2104 <DB_guineaKing@hotmail.com>
2026-07-04 20:15:23 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from aurora.assistant import Aurora
+
+def main():
+    ai = Aurora()
+    ai.run()
+
+if __name__ == "__main__":
+    main()
+
+from .brain import Brain
+from .memory import Memory
